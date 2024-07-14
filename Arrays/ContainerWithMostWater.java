a62cfc31410dca380453a0b7a2339d0bcf54bcd6
lostanush <lostanush01@gmail.com>
2024-07-15 00:10:58 +0530

    Arrays/ContainerWithMostWater.java

--- Arrays/ContainerWithMostWater.java
+++ Arrays/ContainerWithMostWater.java
@@ -1,12 +1,43 @@
+// QUESTION (01)
 import java.util.ArrayList;
 
-public class ContainerWithMostWater {
-    // QUESTION (01)
-    // Container With Most Water !! Flipkart ,+1 ;
+class ContainerWithMostWater {
+    
+    static void MaxWater(ArrayList<Integer> list){
+        int maxwater=0;
+        int lp=0;
+        int rp=list.size()-1;
+        
+        while(lp<rp){   
+            int ht = Math.min(list.get(lp),list.get(rp));
+            int width= rp-lp;
+            int curr = ht* width;
+            maxwater= Math.max(curr,maxwater);
+            if(list.get(lp)<list.get(rp)){
+                lp++;
+            }
+            else rp--;
+        }
+        System.out.println("Maximum Water Contained will be "+ maxwater+ "unit.sq !! ");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World !!");
+        //1,8,6,2,5,4,8,3,7;
+        int[] arr = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        ArrayList<Integer> height = new ArrayList<>();
+        for(int i: arr){
+            height.add(i);
+        }
+        MaxWater(height);
+ 
+    }
+}
+
+// Container With Most Water !! Flipkart ,+1 ;
     /* For a given n lines on x axis, use 2 lines to from a container such that it holds maximum water. */
-    public static void ContainerWithMaxWater(ArrayList<Integer> list){
-        int counter=0;
-        //METHOD 1 :
+    
+      //METHOD 1 :
         /*int maxwater= Integer.MIN_VALUE;
         for(int i=0;i<list.size();i++){
             for(int j=0;j<list.size();j++){
@@ -17,39 +48,3 @@ public class ContainerWithMostWater {
                 counter++;
             }
         }*/
-
-        //METHOD :
-        int maxwater=0;
-        int lp=0;
-        int rp=list.size()-1;
-        while(lp<rp){   
-            int ht = Math.min(list.get(lp),list.get(rp));
-            int width= rp-lp;
-            int currWater = ht* width;
-            maxwater= Math.max(currWater,maxwater);
-            if(list.get(lp)<list.get(rp)){
-                lp++;
-            }
-            else rp--;
-            counter++;
-        }
-        System.out.println("Maximum Water Contained will be "+ maxwater+ "unit.sq !! "+ counter);
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Hello World !!");
-         //1,8,6,2,5,4,8,3,7;
-         ArrayList<Integer> height = new ArrayList<>();
-         height.add(1);
-         height.add(8);
-         height.add(6);
-         height.add(2);
-         height.add(5);
-         height.add(4);
-         height.add(8);
-         height.add(3);
-         height.add(7);
-         ContainerWithMaxWater(height);
- 
-    }
-}
