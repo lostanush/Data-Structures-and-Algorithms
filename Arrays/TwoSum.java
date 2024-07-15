f63923dc6eef58d35d7f30b413023e814bd1331b
lostanush <lostanush01@gmail.com>
2024-07-15 23:34:20 +0530

    two sum

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -60,12 +60,20 @@ public class TwoSum {
 
     public static void main (String args[]){
         
-        int[] arr = {1,2,3,4,5,6};
-        ArrayList<Integer> nums =new ArrayList<>();
-        for(int i: arr){
-            nums.add(i);
+        int[] nums = {1,2,3,4,5,6};
+        ArrayList<Integer> arr =new ArrayList<>();
+        for(int i: nums){
+            arr.add(i);
         }
-        System.out.println(twoSum(nums, 6));
+        System.out.println(twoSum(arr, 6));
+        
+        // 11,12,6,7,8,9,10 - Sorted & Rotated
+        int[] nums2= {11,12,6,8,9,10};
+        ArrayList<Integer> arr2 =new ArrayList<>();
+        for(int i : nums2){
+            arr2.add(i);
+        }
+        System.out.println(twoSum2(arr2, 20));
     }
 }
 
