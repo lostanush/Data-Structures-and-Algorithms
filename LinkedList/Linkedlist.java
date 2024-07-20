3ca15c404a03c0ed48ee5f9d96cc297ce6279f70
lostanush <lostanush01@gmail.com>
2024-07-20 22:17:29 +0530

    Update ll

--- LinkedList/Linkedlist.java
+++ LinkedList/Linkedlist.java
@@ -1,59 +1,51 @@
-//linked list code - written on 4th sem @anushdighore 
-//add 3 - addFirst,addLast,add;
-//remove 3 - removeFirst,removeLast,remove
-
 import java.util.Scanner;
-public class Linkedlist {
-    
-    public static class Node {
-        int data;
-        Node next;
-        Node() {}
-        Node(int data) { 
-            this.data = data; 
-            this.next= null;    
-        }
-        Node(int data, Node next) { 
-            this.data = data; 
-            this.next = next; 
-        }
-        
+
+class Node {
+    int data;
+    Node next;
+    Node() {}
+    Node(int data) {
+        this.data = data;
+        this.next = null;
     }
-    public static Node head;
-    public static Node tail;
-    public static int size;
+    Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+}
+class  LinkedList {
+    Node head;
+    Node tail;
+    int size;
 
-    // Adding node ------------------------------------->
-
-    public void create(int data){
+    public LinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+    public void add(int data) {
         Node newNode = new Node(data);
-        head= tail = newNode;
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
         size++;
     }
-    public void display(){
-        Node ptr= head;
-        System.out.println(" LinkedList :");
-        while (ptr!=null) {
-            System.out.print(" "+ptr.data);
-            ptr= ptr.next;
-        }
-        System.out.println();
-    }
-    
     public void addFirst(int data){
         Node newNode = new Node(data);
         newNode.next = head;
         head= newNode;
         size++;
     }
-    
     public void addLast(int data){
         Node newNode = new Node(data);
         tail.next= newNode;
         tail= newNode;
         size++;
     }
-
     public void add(int idx,int data){
         if(idx==0){
             addFirst(data);
@@ -71,17 +63,13 @@ public class Linkedlist {
         ptr.next= newNode;
         size++;
     }
-
-    // Removing node ------------------------------------->
-
-    public int removeFirst(){
+    public int rmvFirst(){
         int val= head.data;
         head=head.next;
         size--;
         return val;
     }
-
-    public int removeLast(){
+    public int rmv(){
         int val= tail.data;
         Node prev= head;
         //while (ptr.next!=tail) {
@@ -95,329 +83,114 @@ public class Linkedlist {
         size--;
         return val;
     }
-    public void remove(int idx){
+    public void rmvIdx(int idx){
         if(idx==0){
-            removeFirst();
+            rmvFirst();
             return;
         }
-        //Node newNode= new Node(data);
-        Node ptr= head;
-        idx--;
+        else {
+            if (idx == size-1){
+                rmv();
+                return;
+            }
+            else {
+                Node ptr= head;
+                idx--;
         
-        while(idx!=0){
-            idx--;
-            ptr=ptr.next;
-        }
-        ptr.next=ptr.next.next;
-        //ptr.next= newNode;
-        size--;
-    }
-
-    // Reversing node ------------------------------------->
-
-    public void rev(){
-        Node prev= null;
-        Node curr= tail= head;
-        Node next ;
-
-        while (curr!=null){
-            next= curr.next;
-            curr.next= prev;
-            prev= curr;
-            curr= next;
-        }
-        head= prev;
-    }
-
-    //Delete Nth from Ending ------------------------------->
-    public void DelNthFromEnd(int n){
-        Node prev= head;
-        //System.out.println(head.data);
-        // size-n+1 the node to be deleterd !! 
-        /* 
-        int sz= size;
-        int i=1;
-        int itoFind = sz-n;
-        while (i<itoFind) {
-            prev=prev.next;
-            i++;            
-        }
-        prev.next= prev.next.next;
-        return;
-        */
-        if(n== size){
-            removeFirst();
-            return;
-        }else if(n>size || n<1){
-            System.out.println("INvalid index !! ");
-            return;
-        }
-        int sz= size;
-        int prevv = sz-n-1;
-        System.out.println("Deleting "+(prevv+2)+"th Node : ");
-        while (prevv!=0) {
-            prev=prev.next;
-            prevv--;            
-        }
-        prev.next= prev.next.next;
-        return;
+                while(idx!=0){
+                    idx--;
+                    ptr=ptr.next;
+                }
+                ptr.next=ptr.next.next;
+                //ptr.next= newNode;
+                size--;
+            }
+        }        //Node newNode= new Node(data);
         
     }
-
-    public static Node mid(Node head) {
-        if (head == null || head.next == null) {
-            return head;
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
         }
-    
-        Node slow = head;
-        Node fast = head.next;
-    
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-    
-        return slow;
+        System.out.println("null");
     }
 
-    // check if Palindrome------------------------------------->
-    public boolean checkPalindrome(){
-        if(head == null || head.next == null){
-            return true;
-        }
-        // step1 : Find Mid;
-        Node mid= mid(head);
-
-        // step2 : Reverse 2nd Half;
-        Node prev= null;
-        Node curr= mid;
-        Node next ;
-
-        while (curr!=null){
-            next= curr.next;
-            curr.next= prev;
-            prev= curr;
-            curr= next;
-        }
-        // step3 : check Palindrome;
-        Node left=head;
-        Node right=prev;
-
-        while(right!=null){
-            if(left.data!=right.data){
-                return false;
-            }
-            left= left.next;
-            right= right.next;
-        }
-        return true;
-    } 
-
-    //check cycle============  Floyd's CFA
-    public static boolean iscycle(){
-        Node slow= head;
-        Node fast= head;
-
-        while(fast!=null && fast.next != null){
-            slow= slow.next;
-            fast= fast.next.next;
-            if (slow==fast)
-                return true;
-        }
-        return false;
+    public int getSize() {
+        return size;
     }
-    public static void removecylel(){
-        if(Linkedlist.iscycle()){
-            Node slow =head;
-            Node fast= head;
-            Node prev= head;
-            
-            while(slow!=fast){
-                prev=fast;
-                slow=slow.next;
-                fast=fast.next.next;
-            }
-            prev.next=null;
-        }
-    }
+}
 
-    /////////////////////////////////////////////////
-    // MergeSort==============================
-
-    public static Node merge(Node head1, Node head2) {
-        Node mergedLL = new Node(-1);
-        Node temp = mergedLL;
-    
-        while (head1 != null && head2 != null) {
-            if (head1.data <= head2.data) {
-                temp.next = head1;
-                head1 = head1.next;
-            } else {
-                temp.next = head2;
-                head2 = head2.next;
-            }
-            temp = temp.next;
-        }
-    
-        // Add remaining elements from either list
-        if (head1 != null) {
-            temp.next = head1;
-        }
-        if (head2 != null) {
-            temp.next = head2;
-        }
-    
-        return mergedLL.next;
-    }
-
-    public static Node mergeSort(Node head) {
-        if (head == null || head.next == null)
-            return head;
-    
-        // Find the middle of the list
-        Node mid = mid(head);
-        Node rightHead = mid.next;
-        var leftHead = head;
-        mid.next = null; // Splitting the list into two halves
-    
-        // Recursively sort the two halves
-        Node newLeft = mergeSort(leftHead);
-        Node newRight = mergeSort(rightHead);
-    
-        // Merge the sorted halves
-        return merge(newLeft, newRight);
-    }
-
-    public static Node rev(Node mid){
-        Node curr=mid.next;
-        mid.next=null;
-        
-        Node next;
-        Node prev= null;
-
-        while (curr!=null){
-            next= curr.next;
-            curr.next= prev;
-            prev= curr;
-            curr= next;
-        }
-        return prev;
-    }
-
-    public static Node zigzag(){
-        // FInd mid;
-        // reverse second half 
-        // merge (alternate adding)
-        Node zigzag= new Node();
-        zigzag.next= null;
-
-        Node mid = mid(head);
-        Node right= rev(mid);
-        Node left = head;
-        Node nextL;
-        Node nextR;
-
-        while(left!=null && right!=null){
-            nextL=left.next;
-            nextR=right.next;
-            left.next=right;
-            right.next=nextL;
-
-            left=nextL;
-            right=nextR;
-        }
-        return head;
-    }
-
-
-
-
+public class Linkedlist {
     public static void main(String[] args) {
-        Linkedlist ll =new Linkedlist();
-        
-        int ch;
+        LinkedList ll =new LinkedList();
         
         System.out.print(" MenuDriven LinkedList Program::");
         System.out.print("\n __\n");
-    
-    
+        
         while(true){
             System.out.print("\n Menu::\n");
-            System.out.println(" 1. Create       2. View ");
-            System.out.println(" 4. addFirst     5. addLast     6. addIdx");
-            System.out.println(" 7. removeFirst  8. removeLast  9. removeIdx");
+            System.out.println(" 1. add       4. rmv       7. view");
+            System.out.println(" 2. addFirst  5. rmvFirst  8. size");
+            System.out.println(" 3. addIdx    6. rmvIdx   ");
             System.out.print(" Select option = ");
-            try (Scanner sc = new Scanner(System.in)) {
+            @SuppressWarnings("resource")
+            Scanner sc = new Scanner(System.in);
+                int ch;
                 ch= sc.nextInt();
-                
                 System.out.println();
 
                 switch(ch){
    
-                    case 1: {
-                        if(head==null){
-                            System.out.print("  Enter the [0th]idx element : ");
-                            //data=sc.nextInt();
-                            ll.create(sc.nextInt());        
-                            System.out.println("   Element added Successfully !!");
-                        }
-                        else 
-                            System.out.println(" LL is already created !!");
+                    case 1: {  
+                        System.out.print("  Enter the [0th]idx element : ");
+                        ll.add(sc.nextInt());        
+                        System.out.println("   Element added Successfully !!");
                         break;
                     }
                     
                     case 2: {
-                        if(head!=null){
-                            ll.display();
-                        }
-                        else
-                            System.out.println("Linked List not created !! ");
-                        break;
-                    }
-
-                    case 3: {
-
-                    }
-
-                    case 4: {
                         System.out.print("  Enter the [0th]idx element : ");
                         ll.addFirst(sc.nextInt());
                         System.out.println("   Element added Successfully !! ");
                         break;
                     }
 
-                    case 5: {
-                        System.out.print("  Enter the ["+ size +"th]idx element : ");
-                        ll.addLast(sc.nextInt());
-                        System.out.println("   Element added Successfully !! ");
-                        break;
-                    }
-
-                    case 6: {
-                        int idx;
-                        System.out.print(" Enter the idx : ");
-                        idx= sc.nextInt();
-                        if(idx>=size || idx<0){
-                            System.out.println("  Invalid index !!");
-                            break;
-                        }
-                        System.out.print("  Enter the ["+ idx +"th]idx element : ");
-                        if(idx==0)
-                            ll.addFirst(sc.nextInt());
-                        else if(idx==size-1)
-                            ll.addLast(sc.nextInt());
-                        else
-                            ll.add(idx, sc.nextInt());
-                        System.out.println("   Element added Successfully !! ");
-                        break;
-                    }
                     
+                   
+                    // case 6: { 
+                        
+                        
+                    //     int idx;
+                    //     System.out.print(" Enter the idx : ");
+                    //     idx= sc.nextInt();
+                    //     if(idx>=ll.size || idx<0){
+                    //         System.out.println("  Invalid index !!");
+                    //         break;
+                    //     }
+                    //     else { 
+                    //         System.out.print("  Enter the ["+ idx +"th]idx element : ");
+                    //         ll.add(idx);
+                    //     System.out.println("   Element added Successfully !! ");
+                        
+                    //     } break;
+                    // }
+                    case 3: 
+                        //if(head!=null){
+                            ll.display();
+                        //}
+                        //else
+                        //    System.out.println("Linked List not created !! ");
+                        break;
+
+                    // case 8: System.out.println("Size of the list: " + ll.getSize()); break;
+
                     default: {
                         System.out.println(" Invalid option selected !!");
                         break;
                     }
                 }
-            }
-        }        
+                } 
     }
 }
